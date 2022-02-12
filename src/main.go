add4e5effc31f20562d8fec143e2026306e25cea
leedonggyu <zkfmapf123@naver.com>
2022-02-13 01:38:14 +0900

    add constant

--- src/main.go
+++ src/main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"fmt"
+	"golang-cli/src/utils"
 	"os"
+	"os/exec"
 )
-
-var GREETING = "Back Account CLI"
-var MENU = "1. 은행계좌 등록\n2. 은행계좌 검색\n3. 입금\n4. 출금\n5. 은행계좌 삭제\n"
-var SELECT_MENU = "select menu : "
 
 var menu int
 
 func main() {
 
-	// 해당 문자열 입력
-	fmt.Println(GREETING)
-	fmt.Print(MENU)
-	fmt.Print(SELECT_MENU)
-	fmt.Scanf("%d", &menu)
+	var loop = 0
+	var isExit = false
+	for loop < 1 {
+		fmt.Println(utils.MENU_START)
+		fmt.Print(utils.MENU_LIST)
+		fmt.Scanf("%d", &menu)
 
-	switch menu {
-	case 1:
-		fmt.Print("은행계좌 등록")
-	case 2:
-		fmt.Print("은행계좌 검색")
-	case 3:
-		fmt.Print("은행계좌 입금")
-	case 4:
-		fmt.Print("은행계좌 출금")
-	default:
-		fmt.Printf("%d is not valid", menu)
-		os.Exit(0)
+		switch menu {
+		case 1:
+			fmt.Print(utils.REGISTER)
+		case 2:
+			fmt.Print(utils.SEARCH)
+		case 3:
+			fmt.Print(utils.DEPOSIT)
+		case 4:
+			fmt.Print(utils.WITHDRAW)
+		case 5:
+			fmt.Println(utils.DELETE)
+		default:
+			fmt.Printf("%d is not valid", menu)
+			isExit = !isExit
+		}
+
+		if isExit {
+			os.Exit(0)
+		}
+
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		continue
 	}
 
 }
